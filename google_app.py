22cc2e59543da46614515634a43b617589d40993
itsmevishnu <vishnujayadhevan@yahoo.com>
2023-05-01 11:56:53 +0530

    Learning machine learnng using python, analysed the data from the online and following some online tutorials

---
+++ google_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 27 22:21:17 2022
+
+@author: vishnujayan
+"""
+#importing necessary packages
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import seaborn as sns
+
+#read data 
+data = pd.read_csv('/home/vishnujayan/projects/machine_learning/data_set/googleplaystore.csv')
+
+
+
