1c632d8f35746bd3b9215ac041ea13daa531af18
itsmevishnu <vishnujayadhevan@yahoo.com>
2023-05-31 13:03:13 +0530

    modified file gneretor with argparse and more function:

--- file_name_generator.py
+++ file_name_generator.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 
+import argparse
 import sys
+
+"""
+Implementing the parser for named args
+"""
+parser=argparse.ArgumentParser(
+    prog="File Name Generator", 
+    description="This is help to create file name from given text and remove all the unwanted, unsupported characters from the file name"
+    )
+
+parser.add_argument("name", help="The string for the file name. The output string will strip all the unsuppored characters from the string")
+parser.add_argument("--extention", "-e", help="The type of the file. Eg: pdf, jpg, etc.")
+args = parser.parse_args()
 
 """
 Function to generate a valid file name from entered string:wq
 """
-if len(sys.argv) !=2:
-    print(f"File name is not provided. Please provide the file name")
-else:
-    input_text = sys.argv[1]
-    output_text = input_text.replace(' ', '_').replace('\\', '_').replace('-','_')
+if args.name:
+    input_text = args.name
+    unwanted_symbols = "!@#$%^&*(),.:;'|`'~-+=/><?\\\n\t "
+    output_text = "".join("_" if char in unwanted_symbols else char for char in input_text )
+    output_text += f".{args.extention}" if args.extention else ''
     print(f"Generated file name: {output_text}")
+else:
+    print(f"Name is not provided. Please provide the name")
